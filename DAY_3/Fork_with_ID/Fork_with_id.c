1118b76df42592b48ffbb3c8466dc9e4461b1f59
Tejaswani Nikhil <tejaswaninikhil24@gmail.com>
2022-12-08 18:12:35 +0530

    Forking with ID's

---
+++ DAY_3/Fork_with_ID/Fork_with_id.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t id;
+    printf("\n Before Fork....\n");
+    id = fork();
+    printf("\n After Fork\n PID:%d\t PPID:%d\n",getpid(),getppid());
+
+    if(id == 0)
+     {
+        printf("\n Child Process...\n");
+     }
+    else
+     {
+        printf("\n Parent Process...\n");
+     } 
+    return 0;
+}
